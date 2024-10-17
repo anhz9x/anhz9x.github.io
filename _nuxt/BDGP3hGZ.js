b93b2a3429c6557c2f4b31156bbc6b2497b303bb
Anhnt <anhnt.jr@gmail.com>
2024-10-17 10:26:16 +0700

    Init

---
+++ _nuxt/BDGP3hGZ.js
@@ -0,0 +1,1 @@
+import{_ as e,x as n,v as c}from"./CrbFMb8n.js";const r={};function t(o,a){return c(),n("h1",null,"Index page")}const _=e(r,[["render",t]]);export{_ as default};
---
+++ _nuxt/gg0awGPL.js
@@ -0,0 +1,1 @@
+import{_ as a,x as s,y as o,L as l,v as r}from"./CrbFMb8n.js";const n={};function c(t,e){return r(),s("div",null,[e[0]||(e[0]=o("p",null,"Some default layout content shared across all pages",-1)),l(t.$slots,"default")])}const f=a(n,[["render",c]]);export{f as default};
